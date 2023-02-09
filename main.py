d02595ee388bbcf3db474da4afbe6734a43c2525
Zaza <96164075+Zaza2215@users.noreply.github.com>
2023-02-09 23:02:30 +0200

    Add line coding and decoding
    * add four new methods(line_code_i, line_decode_i, line_code, line_decode)

--- main.py
+++ main.py
@@ -1,5 +1,7 @@
 class Coding:
     __KEY = 18
+    __KEY_LINE = 17
+    __KEY_LINE_DECODE = None
     __ALPHABET = 'абвгґдеєжзиіїйклмнопрстуфхцчшщьюя ,._'
 
     def __init__(self, text=None, ctext=None):
@@ -24,4 +26,23 @@ class Coding:
             text_code.append(self.caesar_decode_i(i))
         self.text = ''.join(text_code)
 
+    def line_code_i(self, i: str) -> str:
+        return self.__ALPHABET[(self.__ALPHABET.index(i) * self.__KEY_LINE) % len(self.__ALPHABET)]
 
+    def line_code(self):
+        text_code = []
+        for i in self.text:
+            text_code.append(self.line_code_i(i))
+        self.ctext = ''.join(text_code)
+
+    def line_decode_i(self, i):
+        return self.__ALPHABET[i * self.__KEY_LINE_DECODE % len(self.__ALPHABET)]
+
+    def line_decode(self):
+        text_code = []
+        if not self.__KEY_LINE_DECODE:
+            self.__KEY_LINE_DECODE = self.__KEY_LINE ** (len(self.__ALPHABET) - 2) % len(self.__ALPHABET)
+
+        for i in self.ctext:
+            text_code.append(self.line_decode_i(i))
+        self.text = ''.join(text_code)

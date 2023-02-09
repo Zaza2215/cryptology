6007df7213840583e66dc670b13ee450957aba2a
Zaza <96164075+Zaza2215@users.noreply.github.com>
2023-02-09 23:00:29 +0200

    Refactor caesar coding
    * edit methods caesar_code_i and caesar_decode_i
    * fix error

--- main.py
+++ main.py
@@ -6,20 +6,22 @@ class Coding:
         self.text = text
         self.ctext = ctext
 
-    def caesar_code_i(self, i):
-        return (self.__ALPHABET.index(i) + self.__KEY) % len(self.__ALPHABET)
+    def caesar_code_i(self, i: str) -> str:
+        return self.__ALPHABET[(self.__ALPHABET.index(i) + self.__KEY) % len(self.__ALPHABET)]
 
     def caesar_code(self):
         text_code = []
         for i in self.text:
-            text_code.append(self.__ALPHABET[self.caesar_code_i(i)])
+            text_code.append(self.caesar_code_i(i))
         self.ctext = ''.join(text_code)
 
-    def caesar_decode_i(self, i):
-        return (self.__ALPHABET.index(i) - self.__KEY) % len(self.__ALPHABET)
+    def caesar_decode_i(self, i: str) -> str:
+        return self.__ALPHABET[(self.__ALPHABET.index(i) - self.__KEY) % len(self.__ALPHABET)]
 
     def caesar_decode(self):
         text_code = []
-        for i in self.text:
-            text_code.append(self.__ALPHABET[self.caesar_decode_i(i)])
-        self.ctext = ''.join(text_code)
+        for i in self.ctext:
+            text_code.append(self.caesar_decode_i(i))
+        self.text = ''.join(text_code)
+
+

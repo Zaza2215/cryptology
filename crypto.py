d50bde5a44310349b10f930cc38e52325ebf52ba
Zaza <96164075+Zaza2215@users.noreply.github.com>
2023-02-15 11:14:46 +0200

    Rewritten program using inheritance
    * add three new classes: Caesar, Line, Affine
    * add validation for text, keys, and alphabet
    * delete attribute ctext, now methods code and decode return string type

--- crypto.py
+++ crypto.py
@@ -1,21 +1,22 @@
 from alphabet import Alphabet
+from math import gcd
+
+
+def is_prime(num: int) -> bool:
+    if num % 2 == 0:
+        return num == 2
+    d = 3
+    while d * d <= num and num % d:
+        d += 2
+    return d * d > num
 
 
 class Crypto:
-    __KEY = 18
-    __KEY_LINE = 17
-    __KEY_LINE_DECODE = None
-    __A = 25
-    __A_DECODE = None
-    __B = 17
-    __ALPHABET = Alphabet('abcdefghijklmnopqrstuvwxyz1234567890 ,.?_')
-    __LEN_ALP = len(__ALPHABET)
+    _alp = Alphabet('abcdefghijklmnopqrstuvwxyz1234567890 ,.?_')
+    _len_alp = len(_alp)
 
-    def __init__(self, text=None, ctext=None):
+    def __init__(self, text: str = ''):
         self.text = text
-        self.ctext = ctext
-        self.__KEY_LINE_DECODE = self.__KEY_LINE ** (self.__LEN_ALP - 2) % self.__LEN_ALP
-        self.__A_DECODE = self.__class__.get_inverse_key(self.__A, len(self.__ALPHABET))
 
     @staticmethod
     def get_inverse_key(a, m):
@@ -25,73 +26,148 @@ class Crypto:
         return -1
 
     @property
-    def alphabet(self):
-        return self.__ALPHABET
+    def text(self):
+        return self._text
 
-    @alphabet.setter
-    def alphabet(self, value: str):
+    @text.setter
+    def text(self, value):
+        if isinstance(value, str) and not set(value) - set(self.alp):
+            self._text = value
+        else:
+            raise ValueError(f'text must be string type and all signs of text must be into alphabet: {self.alp}')
+
+    @property
+    def alp(self):
+        return self._alp
+
+    @alp.setter
+    def alp(self, value: str):
 
         if not isinstance(value, str):
             raise ValueError(f'Expected a string but received an {type(value)}')
-        if len(set(value)) != len(value):
+        elif len(set(value)) != len(value):
             raise ValueError('Signs are repeated in alphabet')
         else:
-            self.__ALPHABET = value
-            self.__LEN_ALP = len(value)
+            self._alp = value
+            self._len_alp = len(value)
 
-    def caesar_code_i(self, i: str) -> str:
-        return self.alphabet[(self.alphabet.index(i) + self.__KEY) % self.__LEN_ALP]
 
-    def caesar_code(self):
+class Caesar(Crypto):
+    def __init__(self, text: str, key: int):
+        self.key = key
+        super().__init__(text)
+
+    @property
+    def key(self):
+        return self._key
+
+    @key.setter
+    def key(self, value):
+        if isinstance(value, int):
+            self._key = value
+        else:
+            raise TypeError('Key must be integer')
+
+    def code_i(self, i: str) -> str:
+        return self.alp[(self._alp.index(i) + self.key) % self._len_alp]
+
+    def code(self):
         text_code = []
         for i in self.text:
-            text_code.append(self.caesar_code_i(i))
-        self.ctext = ''.join(text_code)
+            text_code.append(self.code_i(i))
+        return ''.join(text_code)
 
-    def caesar_decode_i(self, i: str) -> str:
-        return self.alphabet[(self.alphabet.index(i) - self.__KEY) % self.__LEN_ALP]
+    def decode_i(self, i: str) -> str:
+        return self.alp[(self._alp.index(i) - self.key) % self._len_alp]
 
-    def caesar_decode(self):
+    def decode(self):
         text_code = []
-        for i in self.ctext:
-            text_code.append(self.caesar_decode_i(i))
-        self.text = ''.join(text_code)
+        for i in self.text:
+            text_code.append(self.decode_i(i))
+        return ''.join(text_code)
 
-    def line_code_i(self, i: str) -> str:
-        return self.alphabet[(self.alphabet.index(i) * self.__KEY_LINE) % self.__LEN_ALP]
+
+class Line(Crypto):
+    def __init__(self, text: str, key: int):
+        super().__init__(text)
+        self.key = key
+        self._key_decode = self.key ** (self._len_alp - 2) % self._len_alp
+
+    @property
+    def key(self):
+        return self._key
+
+    @key.setter
+    def key(self, value):
+        if isinstance(value, int) and gcd(value, self._len_alp) == 1:
+            self._key = value
+        else:
+            raise ValueError('Key must be integer and length of alphabet with key must be coprime')
+
+    def code_i(self, i: str) -> str:
+        return self.alp[(self.alp.index(i) * self.key) % self._len_alp]
 
     # PROBLEM: first sign of alphabet doesn't encrypt
-    def line_code(self):
+    def code(self) -> str:
         text_code = []
         for i in self.text:
-            text_code.append(self.line_code_i(i))
-        self.ctext = ''.join(text_code)
+            text_code.append(self.code_i(i))
+        return ''.join(text_code)
 
-    def line_decode_i(self, i: str) -> str:
-        return self.__ALPHABET[self.alphabet.index(i) * self.__KEY_LINE_DECODE % self.__LEN_ALP]
+    def decode_i(self, i: str) -> str:
+        return self.alp[self.alp.index(i) * self._key_decode % self._len_alp]
 
-    def line_decode(self):
-        text_code = []
-        for i in self.ctext:
-            text_code.append(self.line_decode_i(i))
-        self.text = ''.join(text_code)
-
-    def affine_code_i(self, i: str) -> str:
-        return self.alphabet[(self.alphabet.index(i) * self.__A + self.__B) % self.__LEN_ALP]
-
-    def affine_code(self):
+    def decode(self) -> str:
         text_code = []
         for i in self.text:
-            text_code.append(self.affine_code_i(i))
-        self.ctext = ''.join(text_code)
+            text_code.append(self.decode_i(i))
+        return ''.join(text_code)
 
-    def affine_decode_i(self, i: str) -> str:
-        index = self.__A_DECODE * (self.__ALPHABET.index(i) + len(self.__ALPHABET) - self.__B) % len(
-            self.__ALPHABET)
-        return self.__ALPHABET[index]
 
-    def affine_decode(self):
+class Affine(Crypto):
+    def __init__(self, text: str, key_1: int, key_2: int):
+        super().__init__(text)
+        self.key_1 = key_1
+        self.key_2 = key_2
+
+    @property
+    def key_1(self):
+        return self._key_1
+
+    @key_1.setter
+    def key_1(self, value):
+        if isinstance(value, int) and gcd(value, len(self.alp)) == 1:
+            self._key_1 = value
+            self._key_decode = self.__class__.get_inverse_key(self.key_1, len(self.alp))
+        else:
+            raise ValueError('Key must be integer and length of alphabet with key must be coprime')
+
+    @property
+    def key_2(self):
+        return self._key_2
+
+    @key_2.setter
+    def key_2(self, value):
+        if isinstance(value, int):
+            self._key_2 = value
+        else:
+            raise TypeError('Key must be integer')
+
+    def code_i(self, i: str) -> str:
+        return self.alp[(self.alp.index(i) * self.key_1 + self.key_2) % self._len_alp]
+
+    def code(self):
         text_code = []
-        for i in self.ctext:
-            text_code.append(self.affine_decode_i(i))
-        self.text = ''.join(text_code)
+        for i in self.text:
+            text_code.append(self.code_i(i))
+        return ''.join(text_code)
+
+    def decode_i(self, i: str) -> str:
+        index = self._key_decode * (self.alp.index(i) + len(self.alp) - self._key_2) % len(self.alp)
+        return self.alp[index]
+
+    def decode(self):
+        text_code = []
+        for i in self.text:
+            text_code.append(self.decode_i(i))
+        return ''.join(text_code)
